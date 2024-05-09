e189ea0b56fc9d86e71f8413c645b6ecf57a156a
zzun73 <736300@naver.com>
2024-05-10 08:23:26 +0900

    feat: 회원 매퍼 인터페이스 구현

---
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/member/mapper/MemberMapper.java
@@ -0,0 +1,29 @@
+package com.ssafy.enjoytrip.domain.member.mapper;
+
+import com.ssafy.enjoytrip.domain.member.model.Member;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface MemberMapper {
+
+    void joinMember(Member member); // 회원가입
+
+    Member login(Member member); // 로그인
+
+    void logout(Long memberId); // 로그아웃
+
+    Member userInfo(String memberId); // 회원 상세 정보 조회
+
+    List<Member> listMember(); // 회원 목록 조회
+
+    void modifyMember(Member member); // 회원 정보 수정
+
+    void deleteMember(Member member); // 회원 정보 삭제
+
+    int idCheck(String memberId);// 아이디 중복검사
+
+    int findPassword(Member member);// 비밀번호 찾기
+
+}
