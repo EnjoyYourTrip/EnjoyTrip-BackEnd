d295d0a48a3c6ee5e74b893b1d0dc064ad2d92a9
zzun73 <736300@naver.com>
2024-05-12 14:52:06 +0900

    feat: 질문 게시판 sql 추가

---
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/question/mapper/QuestionMapper.java
@@ -0,0 +1,26 @@
+package com.ssafy.enjoytrip.domain.question.mapper;
+
+import com.ssafy.enjoytrip.domain.question.model.dto.SearchCondition;
+import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.enjoytrip.domain.question.model.Question;
+
+import java.util.List;
+
+@Mapper
+public interface QuestionMapper {
+
+    List<Question> listQuestion();
+
+    Question questionInfo(Long questionId);
+
+    void insertQuestion(Question question);
+
+    void deleteQuestion(Long questionId);
+
+    void updateQuestion(Question question);
+
+    void updateHit(Long questionId);
+
+    List<Question> searchQuestion(SearchCondition searchCondition);
+}
