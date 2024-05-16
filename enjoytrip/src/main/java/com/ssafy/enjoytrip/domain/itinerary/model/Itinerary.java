a62ad1cdb7e1936898ceee8065d35a3cec6fd3d6
남혁준 <gurwns0000@gmail.com>
2024-05-16 14:22:34 +0900

    refactor: 정적 팩토리 메서드 패턴 적용

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/itinerary/model/Itinerary.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/itinerary/model/Itinerary.java
@@ -1,23 +1,37 @@
 package com.ssafy.enjoytrip.domain.itinerary.model;
 
 import com.ssafy.enjoytrip.domain.common.BaseTimeEntity;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Itinerary extends BaseTimeEntity {
 
     private Long itineraryId;
-    private LocalDate startDate;
-    private LocalDate endDate;
     private String title;
     private String content;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private Long memberId;
+    private List<ItineraryDetail> itineraryDetailList;
+
+    private Itinerary(String title, String content, LocalDate startDate, LocalDate endDate, Long memberId, List<ItineraryDetail> itineraryDetailList) {
+        this.title = title;
+        this.content = content;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.memberId = memberId;
+        this.itineraryDetailList = itineraryDetailList;
+    }
+
+    public static Itinerary createItinerary(String title, String content, LocalDate startDate, LocalDate endDate, Long memberId, List<ItineraryDetail> itineraryDetailList) {
+        return new Itinerary(title, content, startDate, endDate, memberId, itineraryDetailList);
+    }
 }
