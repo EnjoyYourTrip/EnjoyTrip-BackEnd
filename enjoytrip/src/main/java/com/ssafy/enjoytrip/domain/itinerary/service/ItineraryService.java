41dcf00302cb585139b8433b37c0e56cf2a967a9
남혁준 <gurwns0000@gmail.com>
2024-05-16 15:50:19 +0900

    feat: 여행 일정 개요 서비스 로직 추가

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/itinerary/service/ItineraryService.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/itinerary/service/ItineraryService.java
@@ -1,4 +1,25 @@
 package com.ssafy.enjoytrip.domain.itinerary.service;
 
+import com.ssafy.enjoytrip.domain.itinerary.model.Itinerary;
+import com.ssafy.enjoytrip.domain.itinerary.model.ItineraryDetail;
+import com.ssafy.enjoytrip.domain.itinerary.model.dto.ItineraryOverviewDto;
+
+import java.util.List;
+
 public interface ItineraryService {
+    void insertItinerary(Itinerary itinerary);
+
+    Long findLastItineraryId(Long memberId);
+
+    List<Itinerary> getItineraryList();
+
+    List<Itinerary> findItineraryList(Long memberId);
+
+    void insertItineraryDetail(ItineraryDetail itineraryDetail);
+
+    List<ItineraryDetail> getItineraryDetailList(Long itineraryId);
+
+    List<ItineraryOverviewDto> getItineraryOverviewList();
+
+    void deleteItinerary(Long itineraryId, Long memberId);
 }
--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/itinerary/service/ItineraryServiceImpl.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/itinerary/service/ItineraryServiceImpl.java
@@ -1,7 +1,64 @@
 package com.ssafy.enjoytrip.domain.itinerary.service;
 
+import com.ssafy.enjoytrip.domain.itinerary.mapper.ItineraryMapper;
+import com.ssafy.enjoytrip.domain.itinerary.model.Itinerary;
+import com.ssafy.enjoytrip.domain.itinerary.model.ItineraryDetail;
+import com.ssafy.enjoytrip.domain.itinerary.model.dto.ItineraryOverviewDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class ItineraryServiceImpl implements ItineraryService {
+    private final ItineraryMapper itineraryMapper;
+
+    @Override
+    public void insertItinerary(Itinerary itinerary) {
+        itineraryMapper.insertItinerary(itinerary);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Long findLastItineraryId(Long memberId) {
+        return itineraryMapper.findLastItineraryId(memberId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Itinerary> getItineraryList() {
+        return itineraryMapper.getItineraryList();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Itinerary> findItineraryList(Long memberId) {
+        return itineraryMapper.findItineraryList(memberId);
+    }
+
+    @Override
+    public void insertItineraryDetail(ItineraryDetail itineraryDetail) {
+        itineraryMapper.insertItineraryDetail(itineraryDetail);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ItineraryDetail> getItineraryDetailList(Long itineraryId) {
+        return itineraryMapper.getItineraryDetailList(itineraryId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ItineraryOverviewDto> getItineraryOverviewList() {
+        return itineraryMapper.getItineraryOverviewList();
+    }
+
+    @Override
+    public void deleteItinerary(Long itineraryId, Long memberId) {
+        itineraryMapper.deleteItineraryDetail(itineraryId);
+        itineraryMapper.deleteItinerary(itineraryId, memberId);
+    }
 }
