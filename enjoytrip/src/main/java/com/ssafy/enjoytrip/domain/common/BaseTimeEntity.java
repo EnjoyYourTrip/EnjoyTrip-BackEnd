98ed7776d3acc8f34deb50c0b71971cbadf966f7
zzun73 <736300@naver.com>
2024-05-18 13:23:47 +0900

    refactor: 생성일, 마지막 수정일 JSON 직렬화하도록 변경

--- enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/common/BaseTimeEntity.java
+++ enjoytrip/src/main/java/com/ssafy/enjoytrip/domain/common/BaseTimeEntity.java
@@ -1,14 +1,18 @@
 package com.ssafy.enjoytrip.domain.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
 
 @Getter
 @Setter
 public class BaseTimeEntity {
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdDate;
+    @JsonFormat(shape = STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime lastModifiedDate;
 }
