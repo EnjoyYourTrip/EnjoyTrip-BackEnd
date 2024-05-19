10b087c89264023d631ed1be20547bacca704ab3
zzun73 <736300@naver.com>
2024-05-20 02:40:59 +0900

    refactor: 핫플레이스 테이블 변경

--- enjoytrip/src/main/resources/schema.sql
+++ enjoytrip/src/main/resources/schema.sql
@@ -1,61 +1,59 @@
-DROP TABLE IF EXISTS members, roles, question, answer, itinerary, itinerary_detail, hotplace, comment CASCADE;
+DROP TABLE IF EXISTS members, roles, question, answer, itinerary, itinerary_detail, hotplace, heart_hotplace CASCADE;
 
 CREATE TABLE `members`
 (
     `member_id`          BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `username`           VARCHAR(40) NOT NULL,
-    `id`                 VARCHAR(40) NOT NULL UNIQUE,
+    `username`           VARCHAR(40)  NOT NULL,
+    `id`                 VARCHAR(40)  NOT NULL UNIQUE,
     `password`           VARCHAR(255) NOT NULL,
-    `nickname`           VARCHAR(40) NOT NULL UNIQUE,
+    `nickname`           VARCHAR(40)  NOT NULL UNIQUE,
     `email`              VARCHAR(255) NOT NULL,
-    `token` VARCHAR(1000) NULL DEFAULT NULL,
+    `token`              VARCHAR(1000) NULL DEFAULT NULL,
     `created_date`       DATETIME DEFAULT CURRENT_TIMESTAMP,
     `last_modified_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE `roles`
 (
-    `role_id`            BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `member_id`          BIGINT      NOT NULL,
-    `role_name`          VARCHAR(20) NOT NULL DEFAULT 'USER' CHECK (role_name IN ('ADMIN', 'USER')),
+    `role_id`   BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `member_id` BIGINT      NOT NULL,
+    `role_name` VARCHAR(20) NOT NULL DEFAULT 'USER' CHECK (role_name IN ('ADMIN', 'USER')),
     FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `hotplace`
 (
     `hotplace_id`        BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `content`            VARCHAR(255)   NOT NULL,
-    `latitude`           DECIMAL(10, 8) NOT NULL,
-    `longitude`          DECIMAL(11, 8) NOT NULL,
-    `hotplace_img`       VARCHAR(255)   NOT NULL,
-    `hit`                INT            NOT NULL,
-    `heart`              INT            NOT NULL,
-    `member_id`          BIGINT         NOT NULL,
-    `created_date`       DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `last_modified_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `member_id`          BIGINT       NOT NULL,
+    `title`              VARCHAR(100) NOT NULL,
+    `content`            VARCHAR(255) NOT NULL,
+    `address`            VARCHAR(200) NOT NULL,
+    `heart`              INT         DEFAULT '0',
+    `save_folder`        VARCHAR(45) DEFAULT NULL,
+    `original_file`      VARCHAR(50) DEFAULT NULL,
+    `save_file`          VARCHAR(50) DEFAULT NULL,
+    `created_date`       DATETIME    DEFAULT CURRENT_TIMESTAMP,
+    `last_modified_date` DATETIME    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE
 );
 
-CREATE TABLE `comment`
+CREATE TABLE `heart_hotplace`
 (
-    `comment_id`         BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `member_id`          BIGINT       NOT NULL,
-    `hotplace_id`        BIGINT       NOT NULL,
-    `content`            VARCHAR(255) NOT NULL,
-    `created_date`       DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `last_modified_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE,
-    FOREIGN KEY (`hotplace_id`) REFERENCES `hotplace` (`hotplace_id`) ON DELETE CASCADE
+    `heart_hotplace_id` BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `hotplace_id`       BIGINT NOT NULL,
+    `member_id`         BIGINT NOT NULL,
+    FOREIGN KEY (`hotplace_id`) REFERENCES `hotplace` (`hotplace_id`) ON DELETE CASCADE,
+    FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE
 );
 
 CREATE TABLE `itinerary`
 (
     `itinerary_id`       BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `start_date`         DATE        NOT NULL,
-    `end_date`           DATE        NOT NULL,
-    `title`              VARCHAR(40) NOT NULL,
-    `content`            TEXT        NOT NULL,
-    `member_id`          BIGINT      NOT NULL,
+    `start_date`         DATE         NOT NULL,
+    `end_date`           DATE         NOT NULL,
+    `title`              VARCHAR(100) NOT NULL,
+    `content`            TEXT         NOT NULL,
+    `member_id`          BIGINT       NOT NULL,
     `created_date`       DATETIME DEFAULT CURRENT_TIMESTAMP,
     `last_modified_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE
@@ -73,13 +71,13 @@ CREATE TABLE `itinerary_detail`
 CREATE TABLE `question`
 (
     `question_id`        BIGINT AUTO_INCREMENT PRIMARY KEY,
-    `title`              VARCHAR(40)  NOT NULL,
+    `title`              VARCHAR(100) NOT NULL,
     `content`            VARCHAR(255) NOT NULL,
-    `hit`                INT                   DEFAULT 0,
+    `hit`                INT          DEFAULT 0,
     `has_response`       BOOLEAN      NOT NULL DEFAULT false,
     `member_id`          BIGINT       NOT NULL,
-    `created_date`       DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `last_modified_date` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `created_date`       DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    `last_modified_date` DATETIME     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (`member_id`) REFERENCES `members` (`member_id`) ON DELETE CASCADE
 );
 
