faa2e7e415d134b610206dd463c8f20717e21d10
zzun73 <736300@naver.com>
2024-05-23 11:33:53 +0900

    chore: data.sql 삭제

--- enjoytrip/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO members(username, id, password, nickname, email, created_date, last_modified_date)
-VALUES ('admin', 'admin', 'admin', 'admin', 'admin', now(), now());
-INSERT INTO roles(member_id, role_name) VALUES (1, 'ADMIN');
